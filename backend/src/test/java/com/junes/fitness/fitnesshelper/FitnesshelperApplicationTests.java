ec361d7d77c3dc1abccb5313163ca2e768a3f766
NuckJunes <najones47@outlook.com>
2024-08-14 10:35:38 -0400

    added intial project files for backend spring application.

---
+++ backend/src/test/java/com/junes/fitness/fitnesshelper/FitnesshelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junes.fitness.fitnesshelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnesshelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
