e6b38cff339dbb2a8657165c5a342723488516b7
NuckJunes <najones47@outlook.com>
2024-10-02 09:18:38 -0400

    Added working links to exercise tab and some basic css.

--- frontend/fitness-helper/src/app/app.routes.ts
+++ frontend/fitness-helper/src/app/app.routes.ts
@@ -1,6 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ExercisePageComponent } from './exercise-page/exercise-page.component';
+import { WorkoutPageComponent } from './workout-page/workout-page.component';
+import { ExerciseTipsPageComponent } from './exercise-tips-page/exercise-tips-page.component';
 
 export const routes: Routes = [
-    {path: '', component: HomeComponent}
+    {path: '', component: HomeComponent},
+    {path: 'exercise', component: ExercisePageComponent},
+    {path: 'workout', component: WorkoutPageComponent},
+    {path: 'exercise-tips', component: ExerciseTipsPageComponent}
 ];
