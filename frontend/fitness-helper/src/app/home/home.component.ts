cdcf520c9ea51e9c2cfc84b5604c7f9df41acd34
NuckJunes <najones47@outlook.com>
2024-10-02 09:01:57 -0400

    Added basic navbar, needs functionality.

--- frontend/fitness-helper/src/app/home/home.component.ts
+++ frontend/fitness-helper/src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavMenuComponent } from "../nav-menu/nav-menu.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NavMenuComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
